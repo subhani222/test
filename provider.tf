afae6613feba47eda3f4a34d2b8520244d42cf5b
subhani222 <mahaboobsubhani593@gmail.com>
2026-03-18 15:47:57 +0530

    provider updated

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Or your preferred version
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # Terraform Cloud will automatically inject:
+  # ARM_CLIENT_ID, ARM_CLIENT_SECRET, etc.
+}
